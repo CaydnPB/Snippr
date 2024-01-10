641c412abf31ca61bebe1b643d67773bb0ea508f
CaydnPB <117025523+CaydnPB@users.noreply.github.com>
2024-01-10 12:52:36 +0000

    Add API functionality

--- Snippr/SnipprURLs/views.py
+++ Snippr/SnipprURLs/views.py
@@ -1,14 +1,28 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import SnipprSnippet
+from django.views import View
+import json
 
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+
+@method_decorator(csrf_exempt, name='dispatch')
 def snippets(request):
-    snippets = SnipprSnippet.objects.all()
-    return render(request, 'projectboard/projects.html', {'projects': projects})
+    if request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        title = data.get('title', '')
+        language = data.get('language', '')
+        code = data.get('code', '')
+        snippet = SnipprSnippet.objects.create(title=title, language=language, code=code)
+        return JsonResponse({'id': snippet.id, 'title': snippet.title, 'language': snippet.language, 'code': snippet.code})
+    else:
+        snippets = SnipprSnippet.objects.all()
+        snippet_list = [{'id': snippet.id, 'title': snippet.title, 'language': snippet.language, 'code': snippet.code} for snippet in snippets]
+        return JsonResponse(snippet_list, safe=False)
 
+@method_decorator(csrf_exempt, name='dispatch')
 def singlesnippet(request, snippet_id):
     snippet = get_object_or_404(SnipprSnippet, pk=snippet_id)
-    return JsonResponse(snippet)
-    return JsonResponse({'message': 'Issue status updated successfully'})
-
-    return render(request, 'projectboard/projectsdetail.html', {'project': project, 'issues_by_status': issues_by_status})
+    snippet_return = {'id': snippet.id, 'title': snippet.title, 'language': snippet.language, 'code': snippet.code}
+    return JsonResponse(snippet_return, safe=False)
